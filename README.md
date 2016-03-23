4cca2bc5304211a6c11c5d062d81040505dc6da8
Giorgio Bonvicini <Giorgio@WIN-GIORGIO>
2016-03-23 23:32:07 +0100

    Version v2.0 Code refactored. Bugs fixed. Fullscreen working.

--- README.md
+++ README.md
@@ -1,12 +1,36 @@
 # MediaControl
 
-Media control is an extremely simple (and bugged - beware!) custom control for JavaFX made by a MediaView and some controls in a bar.
+Media control is an extremely simple custom control for JavaFX composed of a MediaView and some controls in a bar.
+
+NB: The version v2.0 is extremely better than the v1.0: bugs have been fixed, code was polished and general usability is improved.
 
 ## Functionalities
 
-With this control is possible to play a video inside the JavaFX application, pause and resume it, control the volume and put it on fullscreen.
+With this control it is possible to play a video/audio file inside the JavaFX application, pause and resume it, control its volume and watch it fullscreen.
 
-The bugged functionality is the progressbar: it's not interactive (read-only) and does not work while in fullscreen.
+There are lots of constructor which let you setup MediaControl exactly as you need it. You have control over:
+
+ - media file;
+
+ - autoplay;
+
+ - looping (infinte or finite amount of repetitions);
+
+ - time after which the control bar and the cursor are hidden;
+
+ - width of the control (binding);
+
+ - limit of the width (upper limit of the binding);
+
+ - moment on which to start the media;
+
+ - volume of the media.
+
+## Limitations
+
+The progressbar is not interactive: it shows the current time position of the media and the total time, but it isn't possible to skip to a certain point of the media via the progress bar.
+
+In some cases when alternating between fullscreen and normal view the loop count can get screwed up, generally resulting in the media playing some loops more than intended.
 
 ## License
 
--- src/mediacontrol/MediaControl.java
+++ src/mediacontrol/MediaControl.java
@@ -11,6 +11,7 @@ import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
@@ -20,6 +21,7 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaPlayer.Status;
 import javafx.scene.media.MediaView;
@@ -30,12 +32,15 @@ public class MediaControl extends StackPane
     // Default time after which the controls are hidden if the mouse is not moved.
     private final int DEF_WAIT_TO_HIDE = 3;
     
+    private Media media;
     private MediaPlayer mp;
     private javafx.util.Duration mediaDuration;
-    private boolean isFullScreen;
+    private Boolean isAutoplay;
+    private int loopNumber;
     private Timeline checkHideControlBar;
     private Instant lastMoved;
     private int waitHideControlBar;
+    public Boolean isFullScreen = false;
     
     @FXML public MediaView mw;
     @FXML private HBox controlBar;
@@ -48,24 +53,109 @@ public class MediaControl extends StackPane
     @FXML private Slider volSlider;
     
     // Shorthand constructors.
-    public MediaControl(MediaPlayer mediaPlayer)
+    public MediaControl(Media mediaFile)
     {
-        this(mediaPlayer, 0,  null, 0);
+        this(mediaFile, false, 1, 0,  null, 0, 0, 1);
     }    
-    public MediaControl(MediaPlayer mediaPlayer, int waitHideControlBar)
+    public MediaControl(
+            Media mediaFile,
+            boolean autoplay)
     {
-        this(mediaPlayer, waitHideControlBar,  null, 0);
-    }    
-    public MediaControl(MediaPlayer mediaPlayer, int waitHideControlBar, ReadOnlyDoubleProperty widthProp)
+        this(mediaFile, autoplay, 1, 0, null, 0, 0, 1);
+    }
+    public MediaControl(
+            Media mediaFile,
+            boolean autoplay,
+            int loop)
     {
-        this(mediaPlayer, waitHideControlBar, widthProp, 0);
+        this(mediaFile, autoplay, loop, 0, null, 0, 0, 1);
+    }
+    public MediaControl(
+            Media mediaFile,
+            boolean autoplay,
+            int loop,
+            int waitHideControlBar)
+    {
+        this(mediaFile, autoplay, loop, waitHideControlBar, null, 0, 0, 1);
+    }
+    public MediaControl(
+            Media mediaFile,
+            boolean autoplay,
+            int loop,
+            int waitHideControlBar,
+            ReadOnlyDoubleProperty widthProp)
+    {
+        this(mediaFile, autoplay, loop, waitHideControlBar, widthProp, 0, 0, 1);
+    }
+    public MediaControl(
+            Media mediaFile,
+            Boolean autoplay,
+            int loop,
+            int waitHideControlBar,
+            ReadOnlyDoubleProperty widthProp,
+            double maxWidth)
+    {
+        this(mediaFile, autoplay, loop, waitHideControlBar, widthProp, maxWidth, 0, 1);
+    }
+    public MediaControl(
+            Media mediaFile,
+            Boolean autoplay,
+            int loop,
+            int waitHideControlBar,
+            ReadOnlyDoubleProperty widthProp,
+            double maxWidth,
+            double progress)
+    {
+        this(mediaFile, autoplay, loop, waitHideControlBar, widthProp, maxWidth, progress, 1);
     }
     
     // Setup new instance of the MediaControl.
-    public MediaControl(MediaPlayer mediaPlayer, int waitHideControlBar, ReadOnlyDoubleProperty widthProp, float maxWidth)
+    public MediaControl(
+            Media mediaFile,
+            Boolean autoplay,
+            int loop,
+            int waitHideControlBar,
+            ReadOnlyDoubleProperty widthProp,
+            double maxWidth,
+            double progress,
+            double volume)
     {
-        // MediaPlayer is mandatory.
-        mp = mediaPlayer;
+        // Load the FXML structure.
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                getClass().getResource("/mediacontrol/MediaControl.fxml")
+        );
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try
+        {
+            fxmlLoader.load();
+        }
+        catch (IOException exception)
+        {
+            System.err.println("Error loading the /mediacontrol/MediaControl.fxml file!");
+            throw new RuntimeException(exception);
+        }
+        
+        // Setup the Media and MediaPlayer with the given parameters.
+        media = mediaFile;
+        mp = new MediaPlayer(media);
+        isAutoplay = autoplay;
+        mp.setAutoPlay(isAutoplay);
+        mp.setVolume((volume >= 0 && volume <= 1) ? volume : 1);
+        // If loop is negative the media is looped indefinetely
+        loopNumber = loop >= 0 ? loop : MediaPlayer.INDEFINITE;
+        mp.setCycleCount(loopNumber);
+        // When the media ends take it back to the beginning, but check out for loops!
+        mp.setOnEndOfMedia(() -> {
+            if (mp.getCurrentCount() == mp.getCycleCount()
+                    && mp.getCycleCount() != MediaPlayer.INDEFINITE)
+            {
+                mp.seek(javafx.util.Duration.ZERO);
+                mp.pause();
+                playImg.setImage(new Image(getClass().getResource("/mediacontrol/buttonimages/Play.png").toString()));
+            }
+        });
         
         mp.setOnReady(() -> {
              mediaDuration = mp.getCycleDuration();
@@ -83,41 +173,18 @@ public class MediaControl extends StackPane
                         + "/"
                         + secondsToTime((long) mediaDuration.toSeconds()));
             });
+            // Skip to given position.
+            mp.seek(
+                    (progress >= 0 && progress <= 1)
+                    ? mediaDuration.multiply(progress)
+                    : javafx.util.Duration.ZERO
+            );
         });
         
-        // Default is not fullscreen.
-        this.isFullScreen = false;
-        
-        // If user value is 0 or less use the default value.
-        this.waitHideControlBar = waitHideControlBar > 0 ? waitHideControlBar : DEF_WAIT_TO_HIDE;
-        
-        // Set the last time the mouse moved to now.
-        this.lastMoved = Instant.now();
-        
-        // Check every 0.5 seconds if 3 seconds have passed since the mouse last moved.
-        checkHideControlBar = new Timeline(new KeyFrame(javafx.util.Duration.millis(500), ev -> {
-            if (Duration.between(lastMoved, Instant.now()).getSeconds() > this.waitHideControlBar)
-            {
-                // Then hide the control bar.
-                controlBar.setVisible(false);
-            }
-        }));
-        checkHideControlBar.setCycleCount(Timeline.INDEFINITE);
-        // Start checking.
-        checkHideControlBar.play();
-                 
-        // Load the control from FXML.
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MediaControl.fxml"));
-        fxmlLoader.setRoot(this);
-        fxmlLoader.setController(this);
-        try {
-            fxmlLoader.load();
-        } catch (IOException exception) {
-            throw new RuntimeException(exception);
-        }
-        
-        // Setup the width of the control from the paramenters.
+        // Setup the size of MediaView (and then bind size of whole control to it).
         mw.setMediaPlayer(mp);
+        
+        // MediaView width.
         if (widthProp == null)
         {
             // Auto width.
@@ -131,99 +198,101 @@ public class MediaControl extends StackPane
         {
             // Bind width of the control to the given width, but limiting it to the given limit.
             mw.fitWidthProperty().bind(
-                Bindings.when(widthProp.greaterThan(new SimpleDoubleProperty(maxWidth)))
-                    .then(maxWidth)
+                Bindings.when(
+                        widthProp.greaterThan(new SimpleDoubleProperty(maxWidth))
+                    )
+                    .then(new SimpleDoubleProperty(maxWidth))
                     .otherwise(widthProp)
             );
         }
+        // Limit the height of the control to the height of the MediaView.
+        this.maxHeightProperty().bind(mw.fitHeightProperty());
+        
+        // Add control bar (at the bottom of the MediaView).        
         // Bind the width of the control bar to the width of the MediaView.
-        controlBar.maxWidthProperty().bind(mw.fitWidthProperty());
-        controlBar.setMaxHeight(30);
-        
-        // Setup progressSlider.
-        progressSlider.setDisable(true);
-        // Setup buttons.
-        playImg.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> handlePlay(e));
-        fullScrImg.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> handleFullScr(e));
-        resetImg.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> handleReset(e));
-        fullScrImg.setImage(new Image(getClass().getResource("/mediacontrol/buttonimages/FullScr.png").toString()));
-        volImg.setImage(new Image(getClass().getResource("/mediacontrol/buttonimages/Vol.png").toString()));
-        resetImg.setImage(new Image(getClass().getResource("/mediacontrol/buttonimages/Reset.png").toString()));
-        volSlider.setValue(mp.getVolume());
-        volSlider.valueProperty().bindBidirectional(mp.volumeProperty());
-        
-        // When the video ends if it's not still looping take it to the beginning and stop it.
-        mp.setOnEndOfMedia(() -> {
-            if (mp.getCurrentCount() == mp.getCycleCount())
-            {
-                mp.seek(javafx.util.Duration.ZERO);
-                mp.pause();
-                playImg.setImage(new Image(getClass().getResource("/mediacontrol/buttonimages/Play.png").toString()));
-                refreshAll();
-            }
+        mw.boundsInLocalProperty().addListener((obs, old_v,  new_v) -> {
+            controlBar.setMaxWidth(new_v.getWidth());
+            controlBar.setMinWidth(new_v.getWidth());
         });
         
-        if (mp.isAutoPlay() && ! this.isFullScreen)
-        {
-            playImg.setImage(new Image(getClass().getResource("/mediacontrol/buttonimages/Pause.png").toString()));
-        }
+        // Setup the content of the control bar.
         
-        refreshAll();
-    }
-    
-    private void refreshAll()
-    {
-        Status status = mp.getStatus();
-        if (status == Status.UNKNOWN
-                || status == Status.HALTED
-                || status == Status.PAUSED
-                || status == Status.READY
-                || status == Status.STOPPED)
-        {
-            playImg.setImage(new Image(getClass().getResource("/mediacontrol/buttonimages/Play.png").toString()));
-        }
-        else
-        {
-            playImg.setImage(new Image(getClass().getResource("/mediacontrol/buttonimages/Pause.png").toString()));
-        }
-    }
-    
-    
-    // When the mouse is moved the control bar is displayed and the countodwn
-    // to hide it after waitHideControlBar seconds starts.
-    @FXML private void handleMouseMoved()
-    {
-        controlBar.setVisible(true);
+        // Play/pause button.
+        playImg.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> handlePlayButton(e));
+        String action = mp.isAutoPlay() ? "Pause.png" : "Play.png";
+        playImg.setImage(new Image(
+                getClass().getResource("/mediacontrol/buttonimages/" + action).toString()
+        ));
+        
+        // Fullscreen button TODO: add logic
+        fullScrImg.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> handleFullScrButton(e));
+        fullScrImg.setImage(new Image(
+                getClass().getResource("/mediacontrol/buttonimages/FullScr.png").toString()
+        ));
+        
+        // Reset button.
+        resetImg.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> handleResetButton(e));
+        resetImg.setImage(new Image(
+                getClass().getResource("/mediacontrol/buttonimages/Reset.png").toString()
+        ));
+        
+        // Progress slider.
+        progressSlider.setDisable(true);
+        
+        // Volume slider.
+        volImg.setImage(new Image(
+                getClass().getResource("/mediacontrol/buttonimages/Vol.png").toString()
+        ));
+        volSlider.valueProperty().bindBidirectional(mp.volumeProperty());
+        
+        // The control bar should be hidden if the mouse doesn't move for a certain amount of time.
+        
+        // If user value is 0 or less use the default value.
+        this.waitHideControlBar = waitHideControlBar > 0 ? waitHideControlBar : DEF_WAIT_TO_HIDE;
+        // Set the last time the mouse moved to now.
         lastMoved = Instant.now();
+        // Check every 0.3 seconds if 3 seconds have passed since the mouse last moved.
+        // mw.getFitHeight() > 0 is true if a video is shown. With music the control bar is alwas on.
+        checkHideControlBar = new Timeline(new KeyFrame(javafx.util.Duration.millis(300), ev -> {
+            if (Duration.between(lastMoved, Instant.now()).getSeconds() > this.waitHideControlBar
+                    && mw.getFitHeight() > 0)
+            {
+                // Then hide the control bar.
+                controlBar.setVisible(false);
+                this.setCursor(Cursor.NONE);
+            }
+        }));
+        checkHideControlBar.setCycleCount(Timeline.INDEFINITE);
+        // Start checking.
+        checkHideControlBar.play();        
     }
     
-    // Setup button actions.
-    
-    // Play/pause button.
-    @FXML private void handlePlay(MouseEvent e)
+    // Play or pause the media and modify the button accordigly.
+    @FXML private void handlePlayButton(MouseEvent e)
     {
         e.consume();
         Status status = mp.getStatus();
         if (status == Status.UNKNOWN || status == Status.HALTED)
         {
+            playImg.setImage(new Image(
+                    getClass().getResource("/mediacontrol/buttonimages/Play.png").toString()
+            ));
+            System.err.println("Unexpected media status!");
             return;
         }
         if (status == Status.PAUSED
                 || status == Status.READY
                 || status == Status.STOPPED)
         {
-            mp.play();
-            playImg.setImage(new Image(getClass().getResource("/mediacontrol/buttonimages/Pause.png").toString()));
+            playMedia();
         }
         else
         {
-            mp.pause();
-            playImg.setImage(new Image(getClass().getResource("/mediacontrol/buttonimages/Play.png").toString()));
+            pauseMedia();
         }
     }
-        
-    // Fullscreen button
-    @FXML private void handleFullScr(MouseEvent e)
+    
+    @FXML private void handleFullScrButton(MouseEvent e)
     {
         e.consume();
         if (isFullScreen)
@@ -233,42 +302,112 @@ public class MediaControl extends StackPane
         }
         else
         {
-            // Start if not already active.
+            // Start if not active.
+            
+            // We need to recreate a perfect copy of our media.
+            // Get the status.
+            Status currentStatus = mp.getStatus();
+            // Now stop (avoid reproducing again in the background).
+            if (currentStatus == Status.PLAYING)
+            {
+                pauseMedia();
+            }
+            // Get te progress.
+            double progress = mp.getCurrentTime().toMillis()
+                    / mp.getCycleDuration().toMillis();
             Stage stage = new Stage();
-            MediaControl nmc = new MediaControl(mp, waitHideControlBar, stage.widthProperty());
+            // Get all the user values and pass them on.
+            MediaControl nmc = new MediaControl(media,
+                    currentStatus == Status.PLAYING,
+                    loopNumber,
+                    waitHideControlBar,
+                    stage.widthProperty(),
+                    0,
+                    progress,
+                    mp.getVolume());
+            // Set fullscreen.
             nmc.isFullScreen = true;
             Scene scene = new Scene(nmc);
-            // When escape is pressed the window closes.
+            // Close on "Escape".
             scene.setOnKeyPressed(ke -> {
                 if (ke.getCode() == KeyCode.ESCAPE) {
                     stage.close();
                 }
             });
+            // Start!
             stage.setScene(scene);
             stage.setFullScreen(true);
+            
+            // Wait until user closes it.
             stage.showAndWait();
-            refreshAll();
+            
+            // Whene it's closed we need to update our player with values form the closed one.
+            MediaPlayer closedMp = nmc.mw.getMediaPlayer();
+            // Recover status.
+            currentStatus = closedMp.getStatus();
+            closedMp.pause();
+            // Recover progress.
+            progress = closedMp.getCurrentTime().toMillis()
+                    / closedMp.getCycleDuration().toMillis();
+            // Apply status.
+            if (currentStatus == Status.PLAYING)
+            {
+                playMedia();
+            }
+            // Apply progress.
+            mp.seek(mediaDuration.multiply(progress));
+            mp.setVolume(closedMp.getVolume());
         }
     }
     
-    // Reset button
-    @FXML private void handleReset(MouseEvent e)
+    // Take the video to the beginning and pause it.
+    @FXML private void handleResetButton(MouseEvent e)
     {
         e.consume();
         mp.seek(javafx.util.Duration.ZERO);
+        pauseMedia();
+    }
+    
+    // When the mouse is moved show the control bar and update the time.
+    @FXML private void handleMouseMoved()
+    {
+        controlBar.setVisible(true);
+        this.setCursor(Cursor.DEFAULT);
+        lastMoved = Instant.now();
+    }
+    
+    private void pauseMedia()
+    {
         mp.pause();
-        playImg.setImage(new Image(getClass().getResource("/mediacontrol/buttonimages/Play.png").toString()));
+        playImg.setImage(new Image(
+                getClass().getResource("/mediacontrol/buttonimages/Play.png").toString()
+        ));
+    }
+    
+    private void playMedia()
+    {
+        mp.play();
+        playImg.setImage(new Image(
+                getClass().getResource("/mediacontrol/buttonimages/Pause.png").toString()
+        ));
     }
     
     private String secondsToTime(long secs)
     {
         if (secs >= 3600)
         {
-            return String.format("%02d:%02d:%02d", secs/3600, (secs/60)%60, secs%60);
+            return String.format(
+                    "%02d:%02d:%02d",
+                    secs/3600,
+                    (secs/60)%60,
+                    secs%60);
         }
         else
         {
-            return String.format("%02d:%02d", (secs/60)%60, secs%60);
+            return String.format(
+                    "%02d:%02d",
+                    (secs/60)%60,
+                    secs%60);
         }
     }
 }
